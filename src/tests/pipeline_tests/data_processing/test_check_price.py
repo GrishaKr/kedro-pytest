14567705b6451c4185b83c23e2ff908f39b85562
GrishaKr <hryhorii.kravchenko@teck.com>
2021-05-18 13:21:32 +0300

    runner

--- src/tests/pipeline_tests/data_processing/test_check_price.py
+++ src/tests/pipeline_tests/data_processing/test_check_price.py
@@ -9,16 +9,15 @@ class TestCheckPrice:
         self.catalog = data_processing.catalog
         self.pipeline = data_processing.pipeline
         self.path_to_test_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../test_data")
+        self.runner = SequentialRunner()
 
     def test_data_processing_pipeline_max_price(self):
-        runner = SequentialRunner()
-        pipeline_output = runner.run(self.pipeline(), self.catalog)
+        pipeline_output = self.runner.run(self.pipeline(), self.catalog)
         max_values = pipeline_output['master_table'].max()
         assert max_values['price'] == 86150
 
     def test_data_processing_pipeline_min_price(self):
-        runner = SequentialRunner()
-        pipeline_output = runner.run(self.pipeline(), self.catalog)
+        pipeline_output = self.runner.run(self.pipeline(), self.catalog)
         max_values = pipeline_output['master_table'].min()
         assert max_values['price'] == 86150
 
